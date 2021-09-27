cf4e761ab866687a77d9d79e274b042f123d97a3
Antonino Praxedes <antonino.praxedes@gmail.com>
2021-09-26 21:02:18 -0400

    version 1.0

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,43 +8,72 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            updateSellIn(items[i]);
+        for (Item item : items) {
+            updateQuality(item);
+            updateSellIn(item);
         }
+    }
+
+    private void updateQuality(Item item ) {
+        if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            decreaseQualityIfItemHasQuality(item);
+        } else {
+            increaseQualityIncludingBackstagePasses(item);
+        }
+    }
+
+    private void decreaseQualityIfItemHasQuality(Item item) {
+        if (item.quality > 0) {
+            decreaseQuality(item);
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.quality = item.quality - 1;
+        }
+    }
+
+    private void increaseQualityIncludingBackstagePasses(Item item) {
+        if (item.quality < 50) {
+            increaseQuality(item);
+            increaseQualityOfBackstagePasses(item);
+        }
+    }
+
+    private void increaseQualityOfBackstagePasses(Item item) {
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            increaseQualityIfFarFromExpiry(item);
+            increaseQualityIfCloseToExpiry(item);
+        }
+    }
+
+    private void increaseQualityIfCloseToExpiry(Item item) {
+        if (item.sellIn < 6) {
+            increaseQualityIfNotMax(item);
+        }
+    }
+
+    private void increaseQualityIfFarFromExpiry(Item item) {
+        if (item.sellIn < 11) {
+            increaseQualityIfNotMax(item);
+        }
+    }
+
+    private void increaseQualityIfNotMax(Item item) {
+        if (item.quality < 50) {
+            increaseQuality(item);
+        }
+    }
+
+    private void increaseQuality(Item item) {
+        item.quality = item.quality + 1;
     }
 
     private void updateSellIn(Item item) {
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.sellIn = item.sellIn - 1;
         }
-
         handleIfExpired(item);
     }
 
@@ -56,19 +85,17 @@ class GildedRose {
 
     private void handleExpired(Item item) {
         if (!item.name.equals("Aged Brie")) {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                item.quality = item.quality - item.quality;
-            }
+            handleExpiredBackstagePasses(item);
         } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
+            increaseQualityIfNotMax(item);
+        }
+    }
+
+    private void handleExpiredBackstagePasses(Item item) {
+        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            decreaseQualityIfItemHasQuality(item);
+        } else {
+            item.quality = item.quality - item.quality;
         }
     }
 }
